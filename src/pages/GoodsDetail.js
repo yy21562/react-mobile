516f9847ab737fba935773b27828fd1f16b270b8
unknown <1120712591@qq.com>
2019-06-22 21:05:19 +0800

    08-商品详情完成

--- src/pages/GoodsDetail.js
+++ src/pages/GoodsDetail.js
@@ -1,8 +1,19 @@
 import React, { Component, Fragment } from 'react';
+import { NavBar, Icon } from 'antd-mobile';
 class GoodsDetail extends Component {
     render() { 
         return ( 
-1
+            <Fragment>
+              {/* 导航栏 开始 */}
+            <NavBar
+            mode="dark"
+            icon={<Icon type="left" />}
+            onLeftClick={() => console.log('onLeftClick')}
+            onLeftClick={() =>this.props.history.go(-1)}
+          >商品详情
+          </NavBar>
+           {/* 导航栏 结束 */}
+          </Fragment>
         )
     }
 }
